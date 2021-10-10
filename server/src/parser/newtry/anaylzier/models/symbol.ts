6ff0e33e53fd7c7c51ea78bf09f70d927b2a93b6
helsmy <helsmy@example.com>
2021-10-11 01:36:13 +0800

    scoped symbol prototype

--- server/src/parser/newtry/anaylzier/models/symbol.ts
+++ server/src/parser/newtry/anaylzier/models/symbol.ts
@@ -1,4 +1,5 @@
 import { IScoop, ISymbol, ISymType } from '../types';
+import { Range } from 'vscode-languageserver';
 
 export abstract class AHKSymbol implements ISymbol {
 	public readonly name: string;
@@ -12,6 +13,7 @@ export abstract class AHKSymbol implements ISymbol {
 export class VaribaleSymbol extends AHKSymbol {
 	constructor(
 		public readonly name: string,
+		public readonly range: Range,
 		public readonly type: ISymType
 	) {
 		super(name, type);
@@ -33,7 +35,7 @@ export class BuiltinTypeSymbol extends AHKSymbol implements ISymType {
 }
 
 export abstract class ScopedSymbol extends AHKSymbol implements IScoop {
-	private symbols: Map<string, AHKSymbol> = new Map();
+	protected symbols: Map<string, AHKSymbol> = new Map();
 	public readonly enclosingScoop: Maybe<IScoop>;
 
 	constructor(name: string, enclosingScoop?: IScoop) {
@@ -58,13 +60,53 @@ export abstract class ScopedSymbol extends AHKSymbol implements IScoop {
 	}
 }
 
-export class MethodSymbol extends ScopedSymbol {
-	public readonly parameters: ;
-
-	constructor(name: string, param, enclosingScoop?: IScoop) {
+export class AHKMethodSymbol extends ScopedSymbol {
+	constructor(
+		name: string,
+		public readonly range: Range,
+		public readonly requiredParameters: VaribaleSymbol[],
+		public readonly optionalParameters: VaribaleSymbol[],
+		enclosingScoop?: IScoop
+	) {
 		super(name, enclosingScoop);
-		this.parameters = param;
+		this.initParameters();
 	}
 
+	private initParameters() {
+		this.requiredParameters.forEach(v => this.define(v));
+		this.optionalParameters.forEach(v => this.define(v));
+	}
+}
 
+export class AHKObjectSymbol extends ScopedSymbol {
+	/**
+	 * @param name Name of class symbol
+	 * @param range range of symbol
+	 * @param parentScoop parent class
+	 * @param enclosingScoop parent scoop
+	 */
+	constructor(
+		name: string,
+		public readonly range: Range,
+		public readonly parentScoop: Maybe<AHKObjectSymbol>,
+		enclosingScoop?: IScoop
+	) {
+		super(name, enclosingScoop);
+	}
+
+	/**
+	 * Lookup property symbol of a class
+	 * @param name Property symbol name
+	 */
+	resolveProp(name: string): Maybe<ISymbol> {
+		if (this.symbols.has(name))
+			return this.symbols.get(name);
+		let searchScoop = this.parentScoop;
+		while (searchScoop) {
+			const sym = searchScoop.resolve(name);
+			if (sym) return sym;
+			searchScoop = searchScoop.parentScoop;
+		}
+		return undefined;
+	}
 }
